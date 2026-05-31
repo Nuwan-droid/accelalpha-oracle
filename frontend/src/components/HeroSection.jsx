f8fce91db9a7905f9dacf2c456741f57b13ea663
Charith Jayasinghe <153707497+Nuwan-droid@users.noreply.github.com>
2026-05-31 17:07:39 +0530

    fix error

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import accelalphaLogo from "../assets/logo.png";
 import oraclePartnerLogo from "../assets/oracle-partner.png";
 import shipSlideOne from "../assets/oracle-ship-1.jpg";
 import shipSlideTwo from "../assets/oracle-ship-2.jpg";
-import "../styles/heroSection.css";
+import "../styles/herosection.css";
 
 const TARGET_DATE = new Date("2026-11-13T09:30:00");
 
